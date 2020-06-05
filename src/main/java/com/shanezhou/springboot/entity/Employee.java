44a35376589bcd458b1d859d0553eed429c2fd03
shaneZhou <x062982@163.com>
2020-06-05 09:37:09 +0800

    整合MybatisPlus

--- src/main/java/com/shanezhou/springboot/entity/Employee.java
+++ src/main/java/com/shanezhou/springboot/entity/Employee.java
@@ -1,6 +1,10 @@
 package com.shanezhou.springboot.entity;
 
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
@@ -11,17 +15,34 @@ import java.util.Date;
  * @Time 2020/05/30 18:03 下午
  */
 @Data
+@NoArgsConstructor
+@KeySequence(value = "seq_emp_id", clazz = Integer.class)
 public class Employee {
 
-    private int id;
+    @TableId(type = IdType.INPUT)
+    private Integer id;
+    @TableField(value = "username")
     private String userName;
-    private int deptId;
+    @TableField(fill = FieldFill.INSERT)
+    private Integer deptId;
+    @TableField(fill = FieldFill.INSERT)
     private String password;
     private String email;
-    private int age;
-    private int gender;
+    private Integer age;
+    private Integer gender;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthday;
+    @TableField(exist = false)
     private Department department;
+    @Version
+    private Integer version;
 
+    public Employee(String userName) {
+        this.userName = userName;
+    }
+
+    public Employee(String userName, Integer deptId) {
+        this.userName = userName;
+        this.deptId = deptId;
+    }
 }
